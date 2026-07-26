69b28346d85d7246a1207b8becc45684c1b07003
daemonnd <daemonnd@protonmail.com>
2026-07-26 18:30:28 +0200

    refactor(template-creation): move metadata write out of create.py
    The metadata write to the .toml file has been removed because a shared
    database for each template is now used in order to make it easier to
    search and list prompt templates metadata.

--- src/prompt_manager/constants.py
+++ src/prompt_manager/constants.py
@@ -1,6 +1,7 @@
 from platformdirs import user_data_dir
 from pathlib import Path
 
-PROMPT_TEMPLATES: Path = Path(user_data_dir("prompt_library")) / "templates"
-PROMPTS_DIR: Path = Path(user_data_dir("prompt_library")) / "prompts"
-DATABASE_DIR: Path = Path(user_data_dir("prompt_library")) / "templates.db"
+DATA_DIR: Path = Path(user_data_dir("prompt_library"))
+PROMPT_TEMPLATES: Path = DATA_DIR / "templates"
+PROMPTS_DIR: Path = DATA_DIR / "prompts"
+DATABASE_DIR: Path = DATA_DIR / "templates.db"
--- src/prompt_manager/features/create.py
+++ src/prompt_manager/features/create.py
@@ -15,8 +15,8 @@ class CreateTemplate:
             f"How would you describe the new prompt template '{name}'? "
         )
         tags = input(
-            f"Which tags should the new prompt template '{name}' get? Format: 'summary,transcript,video'"
-        ).split(",")
+            f"Which tags should the new prompt template '{name}' get? Format: 'summary,transcript,video' "
+        )
         prompt_file_name = f"{name}.md"
         prompt = input("Enter the prompt template, use '{}' for variables: ")
         return PromptTemplateModel(
@@ -27,40 +27,17 @@ class CreateTemplate:
             prompt=prompt,
         )
 
-    def save_template(self, data: PromptTemplateModel):
-        prompt_template_path: Path = PROMPT_TEMPLATES / f"{data.name}.toml"
-        try:
-            with open(prompt_template_path, "w") as f:
-                f.write(toml.dumps(MetadataModel.from_template(data).model_dump()))
-        except UnicodeDecodeError as e:
-            print(f"Unable to decode TOML in {prompt_template_path}: {str(e)}")
-        except IsADirectoryError as e:
-            print(f"The config file is a directory: {str(e)}")
-        except FileNotFoundError as e:
-            print(
-                f"The config file has not been found at {prompt_template_path}: {str(e)}"
-            )
-        except PermissionError as e:
-            print(
-                f"Permission Error while opening the config file at {prompt_template_path}: {str(e)}"
-            )
-
-        prompt_path: Path = PROMPTS_DIR / data.prompt_file_name
+    def save_prompt(self, prompt: str, prompt_file_name: str | Path):
+        """
+        Method to save the prompt template itselft to a file
+        """
+        prompt_path: Path = PROMPTS_DIR / prompt_file_name
         try:
             with open(prompt_path, "w") as f:
-                f.write(data.prompt)
-        except IsADirectoryError as e:
-            print(f"The config file is a directory: {str(e)}")
-        except FileNotFoundError as e:
-            print(f"The config file has not been found at {prompt_path}: {str(e)}")
-        except PermissionError as e:
-            print(
-                f"Permission Error while opening the config file at {prompt_path}: {str(e)}"
-            )
-
-    def create(self):
-        self.save_template(self.get_data())
-
-
-if __name__ == "__main__":
-    save_template(get_data())
+                f.write(prompt)
+        except IsADirectoryError:
+            raise
+        except FileNotFoundError:
+            raise
+        except PermissionError:
+            raise
