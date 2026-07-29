1f4d05f86051dc8330b3225c60c50c65b2000dbe
daemonnd <daemonnd@protonmail.com>
2026-07-29 18:19:44 +0200

    feat(create-template): add PromptSessions to creation

--- src/prompt_manager/features/create.py
+++ src/prompt_manager/features/create.py
@@ -1,6 +1,12 @@
+from prompt_manager.db_repo import PromptTemplateRepository
+from prompt_manager.inputs.prompts import (
+    get_description_session,
+    get_prompt_session,
+    get_tags_session,
+    get_template_session,
+)
 from prompt_manager.models import MetadataModel, PromptTemplateModel
 from pathlib import Path
-import toml
 from prompt_manager.constants import PROMPT_TEMPLATES, PROMPTS_DIR
 
 
@@ -10,22 +16,54 @@ class CreateTemplate:
         Function that returns the metadata of the template as
         template data model and the template prompt as str
         """
-        name = input("How should the new prompt template be called? ")
-        description = input(
-            f"How would you describe the new prompt template '{name}'? "
-        )
-        tags = input(
-            f"Which tags should the new prompt template '{name}' get? Format: 'summary,transcript,video' "
-        )
-        prompt_file_name = f"{name}.md"
-        prompt = input("Enter the prompt template, use '{}' for variables: ")
-        return PromptTemplateModel(
-            name=name,
-            description=description,
-            tags=tags,
-            prompt_file_name=prompt_file_name,
-            prompt=prompt,
-        )
+        with PromptTemplateRepository() as template_db:
+            names = template_db.list_templates(["name"])
+            existing_names: list[str] = []
+            for entry in names:
+                existing_names.append(entry["name"])
+            name = get_template_session(existing_names).prompt(
+                "How should the new prompt template be called? "
+            )
+            description: str | None = get_description_session().prompt(
+                f"How would you describe the new prompt template '{name}'? (Optional, ENTER to skip) "
+            )  # returns a string, at that point description is a string and not None
+            if description.replace(" ", "") == "":
+                description = None
+            tags = get_tags_session(
+                [
+                    "summary",
+                    "summarization",
+                    "favourite",
+                    "ai",
+                    "transcript",
+                    "video",
+                    "coding",
+                    "assistant",
+                    "finances",
+                    "work",
+                    "school",
+                    "fitness",
+                    "sports",
+                    "health",
+                    "research",
+                    "agent",
+                    "traveling",
+                    "technology",
+                ]
+            ).prompt(
+                f"Which tags should the new prompt template '{name}' get? \nFormat: 'summary,transcript,video' "
+            )
+            prompt_file_name = f"{name}.md"
+            prompt = get_prompt_session().prompt(
+                "Enter the prompt template, use '{}' for variables: \n"
+            )
+            return PromptTemplateModel(
+                name=name,
+                description=description,
+                tags=tags,
+                prompt_file_name=prompt_file_name,
+                prompt=prompt,
+            )
 
     def save_prompt(self, prompt: str, prompt_file_name: str | Path):
         """
