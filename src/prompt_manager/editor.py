538f6b11bc1ec16e8dd96305320227485a5d86d3
daemonnd <daemonnd@protonmail.com>
2026-09-01 17:40:46 +0200

    fix(editor-input): make editor input work

--- src/prompt_manager/editor.py
+++ src/prompt_manager/editor.py
@@ -1,27 +1,35 @@
+import os
+import shlex
+import subprocess
 import tempfile
 from pathlib import Path
-import shlex
-import os
-import subprocess
+from typing import Self
 
 from prompt_manager.errors import EditorError
 
 
 class EditorInput:
-    def __init__(self) -> None:
-        pass
+    """
+    Class for getting input from an editor.
+    Has to be used within a context manager.
+    """
+
+    def __enter__(self) -> Self:
+        with tempfile.NamedTemporaryFile(
+            mode="w",
+            encoding="utf-8",
+        ) as temp_file:
+            self.temp_file_path = temp_file.name
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
+        Path(self.temp_file_path).unlink(missing_ok=True)
 
     def get_input(self) -> str:
-        temp_file = tempfile.NamedTemporaryFile(mode="w", encoding="utf-8")
-        temp_file_path = temp_file.name
-        try:
-            command = self._get_editor_command()
-            command.append(temp_file_path)
-            print(f"command: {command}")
-            self._open_editor(command)
-            return self._read_result(temp_file_path)
-        finally:
-            Path(temp_file_path).unlink()
+        command = self._get_editor_command()
+        command.append(self.temp_file_path)
+        self._open_editor(command)
+        return self._read_result(self.temp_file_path)
 
     def _get_editor_command(self) -> list[str]:
         editor = os.environ.get("EDITOR")
@@ -31,9 +39,12 @@ class EditorInput:
 
     def _open_editor(self, command: list[str]):
         try:
-            result = subprocess.run(args=command, check=True, capture_output=True)
+            subprocess.run(
+                args=command,
+                check=True,
+            )
         except subprocess.CalledProcessError as e:
-            raise EditorError(f"Failed to open editor for writing: {str(e)}")
+            raise EditorError(f"Failed to open editor for writing: {e!s}")
 
     def _read_result(self, temp_file_path) -> str:
         try:
--- src/prompt_manager/features/create.py
+++ src/prompt_manager/features/create.py
@@ -1,14 +1,14 @@
 import logging
-from rich import print as rprint
+from pathlib import Path
 
-from prompt_toolkit.validation import ValidationError
 from prompt_toolkit.document import Document
-
+from prompt_toolkit.validation import ValidationError
+from rich import print as rprint
 
 from prompt_manager.config.models import AppConfig
 from prompt_manager.db_repo import PromptTemplateRepository
 from prompt_manager.editor import EditorInput
-from prompt_manager.features.errors import PromptFileWritingError
+from prompt_manager.features.errors import PromptFileWritingError, TemplateCreationError
 from prompt_manager.inputs.prompts import (
     get_description_session,
     get_prompt_session,
@@ -17,7 +17,6 @@ from prompt_manager.inputs.prompts import (
 )
 from prompt_manager.inputs.validators import PlaceholderValidator
 from prompt_manager.models import PromptTemplateModel
-from pathlib import Path
 from prompt_manager.paths import PROMPTS_DIR
 
 logger = logging.getLogger(__name__)
@@ -34,6 +33,7 @@ class CreateTemplate:
         """
         with PromptTemplateRepository() as template_db:
             names = template_db.list_templates(["name"])
+            prompt: str | None = None
             existing_names: list[str] = []
             for entry in names:
                 existing_names.append(entry["name"])
@@ -75,22 +75,35 @@ class CreateTemplate:
                     "Enter the prompt template, use '{}' for variables: \n"
                 )
             else:
-                prompt = EditorInput().get_input()
-                for i in range(5):
-                    rprint(
-                        "[yellow]Your editor will open. \nEnter the prompt for the prompt template, use '{}' for placeholders.\nWhen done, save and exit.[/yellow]"
-                    )
-                    # input("Press <ENTER> to open the editor...")
-                    try:
-                        prompt = EditorInput().get_input()
-                        PlaceholderValidator().validate(document=Document(text=promp))
-                    except ValidationError as e:
-                        logger.warning(
-                            f"Failed to create a valid prompt because of the following error: {str(e)}",
-                            exc_info=True,
+                rprint(
+                    "[yellow]Your editor will open. \nEnter the prompt for the prompt template, use '{}' for placeholders.\nWhen done, save and exit.[/yellow]"
+                )
+                with EditorInput() as editor:
+                    for i in range(5):
+                        input(
+                            f"Press <ENTER> to open the editor... (attempts remaining: {5 - i}) "
                         )
+                        try:
+                            prompt = editor.get_input()
+                            PlaceholderValidator().validate(
+                                document=Document(text=prompt)
+                            )
+                        except ValidationError as e:
+                            logger.warning(
+                                f"Failed to create a valid prompt because of the following error: {e!s}",
+                                exc_info=True,
+                            )
+                        else:
+                            break
                     else:
-                        break
+                        raise TemplateCreationError(
+                            "Failed to get the prompt because of previous errors"
+                        )
+
+            if prompt is None:
+                raise TemplateCreationError(
+                    "Failed to create template because the prompt is missing"
+                )
 
             return PromptTemplateModel(
                 name=name,
@@ -112,9 +125,9 @@ class CreateTemplate:
                 f.write(prompt)
         except IsADirectoryError as e:
             raise PromptFileWritingError(
-                f"Could not open '{prompt_path}', because it is a dirctory: {str(e)}"
+                f"Could not open '{prompt_path}', because it is a dirctory: {e!s}"
             ) from e
         except PermissionError as e:
             raise PromptFileWritingError(
-                f"Could not write to '{prompt_path}', because of a permission error: {str(e)}"
+                f"Could not write to '{prompt_path}', because of a permission error: {e!s}"
             ) from e
